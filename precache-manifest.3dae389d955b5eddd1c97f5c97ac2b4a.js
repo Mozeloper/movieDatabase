91e19b48de4c5ef6a58d88c123dae9d360287ecf
Mozeloper <ojeimoses@gmail.com>
2020-07-04 13:44:04 +0100

    My Movie Database extracted from RMDB API

---
+++ precache-manifest.3dae389d955b5eddd1c97f5c97ac2b4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4390c3735f25160d60b1cf4e68ab0bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea571fc8701176d9b32",
+    "url": "/static/js/2.9886eec7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9886eec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc6f14ee074af9990af",
+    "url": "/static/js/main.237a7c56.chunk.js"
+  },
+  {
+    "revision": "7f77fac9c633fa4608f2",
+    "url": "/static/js/runtime-main.2fb2fdd6.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
